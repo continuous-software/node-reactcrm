1c8dedee85c4b99ba6a9baed729e42e899b3452b
Amine Mouafik <amine@mouafik.fr>
2014-07-30 00:13:10 +0700

    0.0.5

--- index.js
+++ index.js
@@ -82,77 +82,61 @@ NodeSDK.prototype.registerProspect = function(prospect, callback) {
   });
 };
 
-NodeSDK.prototype.processOrder = function(options, order, prospect, callback) {
-  var req = require('request');
-  console.log('[react] processing order: %s', JSON.stringify(order));
-  this.processOrderWithRocketgate(options, order, prospect, callback);
-  //this.processOrderWithVirtualMerchant(options, order, prospect, callback);
-};
-
-NodeSDK.prototype.processOrderWithRocketgate = function(options, order, prospect, callback) {
+NodeSDK.prototype.processOrderWithRocketgate = function(gateway, offer, prospect, creditcard, callback) {
   var GatewayService = rocketgate.GatewayService;
   var GatewayRequest = rocketgate.GatewayRequest;
   var GatewayResponse = rocketgate.GatewayResponse;
   var service = new GatewayService();
   var request = new GatewayRequest();
   var response = new GatewayResponse();
-  request.Set(GatewayRequest.MERCHANT_ID, options.merchant_id);
-  request.Set(GatewayRequest.MERCHANT_PASSWORD, options.merchant_password);
-  request.Set(GatewayRequest.CARDNO, order.number.trim());
-  var expiry = order.expiry.split('/');
+  request.Set(GatewayRequest.MERCHANT_ID, gateway.merchant_id);
+  request.Set(GatewayRequest.MERCHANT_PASSWORD, gateway.merchant_password);
+  request.Set(GatewayRequest.CARDNO, creditcard.number.trim());
+  var expiry = creditcard.expiration.split('/');
   request.Set(GatewayRequest.EXPIRE_MONTH, expiry[0].trim());
   request.Set(GatewayRequest.EXPIRE_YEAR, expiry[1].trim());
-  request.Set(GatewayRequest.AMOUNT, 42.00);
+  request.Set(GatewayRequest.AMOUNT, offer.amount);
   request.Set(GatewayRequest.MERCHANT_CUSTOMER_ID, prospect.id);
   request.Set(GatewayRequest.MERCHANT_INVOICE_ID, "ReactCRMInv-1");
   request.Set(GatewayRequest.CUSTOMER_FIRSTNAME, prospect.firstname);
   request.Set(GatewayRequest.CUSTOMER_LASTNAME, prospect.lastname);
-  request.Set(GatewayRequest.BILLING_ADDRESS, prospect.address1);
-  request.Set(GatewayRequest.BILLING_CITY, prospect.city);
-  request.Set(GatewayRequest.BILLING_STATE, prospect.state);
-  request.Set(GatewayRequest.BILLING_ZIPCODE, prospect.zipcode);
-  request.Set(GatewayRequest.BILLING_COUNTRY, prospect.country);
+  request.Set(GatewayRequest.BILLING_ADDRESS, prospect.billing.address);
+  request.Set(GatewayRequest.BILLING_CITY, prospect.billing.city);
+  request.Set(GatewayRequest.BILLING_ZIPCODE, prospect.billing.zipcode);
+  request.Set(GatewayRequest.BILLING_COUNTRY, prospect.billing.country);
+  request.Set(GatewayRequest.BILLING_STATE, prospect.billing.region);
   request.Set(GatewayRequest.AVS_CHECK, "IGNORE");
-  request.Set(GatewayRequest.CVV2, order.cvc);
+  request.Set(GatewayRequest.CVV2, creditcard.cvv2);
   request.Set(GatewayRequest.CVV2_CHECK, "IGNORE");
   request.Set(GatewayRequest.EMAIL, prospect.email);
-  request.Set(GatewayRequest.IPADDRESS, "68.224.133.117");
+  request.Set(GatewayRequest.IPADDRESS, prospect.ip_address);
   request.Set(GatewayRequest.SCRUB, "IGNORE");
   service.SetTestMode(true);
   var self = this;
-  order.prospect_id = prospect.id;
-  console.log(prospect);
+  var order = {
+    gateway_id: gateway.id,
+    prospect_id: prospect.id,
+    offer_id: offer.id,
+    amount: offer.amount,
+    ip_address: prospect.ip_address,
+    referer: prospect.referer,
+    creditcard: JSON.stringify(creditcard)
+  };
   service.PerformPurchase(request, response, function(status) {
     if (status) {
-      console.log("Purchase succeeded");
-      console.log("GUID: " + response.Get(GatewayResponse.TRANSACT_ID));
-      console.log("Response Code: " + response.Get(GatewayResponse.RESPONSE_CODE));
-      console.log("Reason Code: " + response.Get(GatewayResponse.REASON_CODE));
-      console.log("AuthNo: " + response.Get(GatewayResponse.AUTH_NO));
-      console.log("AVS: " + response.Get(GatewayResponse.AVS_RESPONSE));
-      console.log("CVV2: " + response.Get(GatewayResponse.CVV2_CODE));
-      console.log("CardHash: " + response.Get(GatewayResponse.CARD_HASH));
-      console.log("CardIssuer: " + response.Get(GatewayResponse.CARD_ISSUER_NAME));
-      console.log("Account: " + response.Get(GatewayResponse.MERCHANT_ACCOUNT));
-      console.log("Scrub: " + response.Get(GatewayResponse.SCRUB_RESULTS));
-      order.billing_status = 'success';
+      order.billing_status = 'completed';
       order.transaction_id = response.Get(GatewayResponse.TRANSACT_ID);
-      order.token = response.Get(GatewayResponse.CARD_HASH);
       self.process(NodeSDK.ACTION_ADD_ORDER, order, function (error, result) {
         callback && callback(null, order.transaction_id);
       });
     }
     else {
+      console.log(response);
       order.billing_status = 'failed';
+      order.gateway_response = response.Get(GatewayResponse.REASON_CODE);
       self.process(NodeSDK.ACTION_ADD_ORDER, order, function (error, result) {
         callback && callback("Purchase failed!");
       });
-      console.log("Purchase failed");
-      console.log("GUID: " + response.Get(GatewayResponse.TRANSACT_ID));
-      console.log("Response Code: " + response.Get(GatewayResponse.RESPONSE_CODE));
-      console.log("Reason Code: " + response.Get(GatewayResponse.REASON_CODE));
-      console.log("Exception: " + response.Get(GatewayResponse.EXCEPTION));
-      console.log("Scrub: " + response.Get(GatewayResponse.SCRUB_RESULTS));
     }
   });
 };
@@ -166,8 +150,8 @@ NodeSDK.prototype.processOrderWithVirtualMerchant = function(options, order, pro
   });
   gateway.doPurchase({
     card_number: order.number,
-    exp_date: order.expiry.split(' / ').join(''),
-    amount: '0.42'
+    exp_date: order.expiration.split(' / ').join(''),
+    amount: order.amount
   }, function(error, result) {
     if (error)
       return callback && callback(error);
