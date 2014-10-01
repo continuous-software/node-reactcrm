886d87c51044a6b6aa8913f21914faac176a426a
Amine Mouafik <amine@mouafik.fr>
2014-10-01 17:27:24 +0700

    Removing useless demo from repository.

--- demo/routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.setup = function(app, controllers) {
-  app.get('/', controllers.index);
-  app.post('/', controllers.registerProspect);
-  app.get('/bill', controllers.bill);
-  app.post('/bill', controllers.processOrder);
-  app.get('/confirmation', controllers.confirmation);
-};
