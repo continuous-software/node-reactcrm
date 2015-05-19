a9eb67ee1abb945e072568bb6b8969c8ecb2f518
Amine Mouafik <amine@mouafik.fr>
2015-05-19 14:31:06 +0700

    refactor(schema): api endpoint retrieval

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ var P = require('bluebird');
 
 module.exports = {
   getAuthenticatedApplication: function (apiKey, apiSecret, options) {
-
-
     var httpGet = P.promisify(request.get);
-    var schemaUrls = process.env.NODE_ENV === 'production' ? 'https://api.reactcrm.com/public/schema.json' : 'http://127.0.0.1:8001/public/schema.json';
-
+    var schemaUrls = options.endpoint + '/public/schema.json';
     return (options.schema ? P.resolve(new ReactCRM(apiKey, apiSecret, options)) :
       httpGet(schemaUrls,{json:true})
         .spread(function (res, schema) {
