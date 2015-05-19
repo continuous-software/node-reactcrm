c2a54885ba06adef503fbbe498a21bf16a63e5f6
lorenzofox3 <laurent34azerty@gmail.com>
2015-05-19 13:03:55 +0700

    give ability to fetch schema dynamically if not provided

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 'use strict';
 
 var ReactCRM = require('./lib/ReactCRM.js');
+var request = require('request');
+var P = require('bluebird');
 
 module.exports = {
-    getAuthenticatedApplication: function (apiKey, apiSecret, options) {
-        var service = new ReactCRM(apiKey, apiSecret, options);
+  getAuthenticatedApplication: function (apiKey, apiSecret, options) {
+
+
+    var httpGet = P.promisify(request.get);
+    var schemaUrls = process.env.NODE_ENV === 'production' ? 'https://api.reactcrm.com/schema.json' : 'http://localhost:8001/schema.json';
+
+    return (options.schema ? P.resolve(new ReactCRM(apiKey, apiSecret, options)) :
+      httpGet(schemaUrls)
+        .then(function (schema) {
+          options.schema = schema;
+          return new ReactCRM(apiKey, apiSecret, options);
+        }))
+      .then(function (service) {
         return service.authenticate();
-    },
-    ReactCRM: ReactCRM
+      })
+  },
+  ReactCRM: ReactCRM
 };
